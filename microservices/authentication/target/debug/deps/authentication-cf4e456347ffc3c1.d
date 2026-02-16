da78761a6cc66cde47f11f22a8fd1de8e3986144
Joki <josedavidamayar@gmail.com>
2026-02-15 20:12:24 -0500

    fix: revome target and cargo.lock from git (should be ignored)

--- microservices/authentication/target/debug/deps/authentication-cf4e456347ffc3c1.d
+++
@@ -1,9 +0,0 @@
-/Users/joki/Documents/code/microservices-fp/microservices/authentication/target/debug/deps/libauthentication-cf4e456347ffc3c1.rmeta: src/main.rs Cargo.toml
-
-/Users/joki/Documents/code/microservices-fp/microservices/authentication/target/debug/deps/authentication-cf4e456347ffc3c1.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
