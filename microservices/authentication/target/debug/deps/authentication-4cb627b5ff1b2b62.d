0a62bac984d7ab74268f6415ddd53e38a23ae332
Joki <josedavidamayar@gmail.com>
2026-02-15 19:54:20 -0500

    feat: init rust project for the auth service

---
+++ microservices/authentication/target/debug/deps/authentication-4cb627b5ff1b2b62.d
@@ -0,0 +1,9 @@
+/Users/joki/Documents/code/microservices-fp/microservices/authentication/target/debug/deps/libauthentication-4cb627b5ff1b2b62.rmeta: src/main.rs Cargo.toml
+
+/Users/joki/Documents/code/microservices-fp/microservices/authentication/target/debug/deps/authentication-4cb627b5ff1b2b62.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
